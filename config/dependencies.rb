183e99849654bd6b8fb9b971637f348f028bba54
Saimon Moore <saimon@saimonmoore.net>
2008-11-28 12:42:00 +0100

    Adding do_mysql (+ deps) to dependencies.rb

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.3"
-dm_gems_version   = "0.9.7"
+merb_gems_version     = "1.0.3"
+dm_gems_version       = "0.9.7"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -22,3 +22,8 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
+
+dependency "addressable",  "2.0.0"
+dependency "data_objects", "0.9.9"
+dependency "ext_lib",      "0.9.8"
+dependency "do-mysql",     "0.9.9"
