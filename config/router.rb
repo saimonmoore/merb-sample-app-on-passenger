4c5c7458c8e54452862e76e840f09527f6ff13e4
Saimon Moore <saimon@saimonmoore.net>
2008-11-28 12:38:27 +0100

    Removing dup resources call in router

--- config/router.rb
+++ config/router.rb
@@ -27,10 +27,8 @@
 
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do
+  # RESTful routes  
   resources :posts
-  resources :posts
-  # RESTful routes
-  # resources :posts
   
   # Adds the required routes for merb-auth using the password slice
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
