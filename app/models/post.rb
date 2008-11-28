fe5ca0561f64687d76f38e6979d7fe684b81cf63
Saimon Moore <saimon@saimonmoore.net>
2008-11-28 12:38:00 +0100

    Adding basic properties to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post
   include DataMapper::Resource
   
   property :id, Serial
-
+  property :title, String
+  property :body, Text
+  
+  property :created_at, DateTime
+  property :created_on, Date
+  property :updated_at, DateTime
+  property :updated_on, Date
+  
+  belongs_to :user
 
 end
